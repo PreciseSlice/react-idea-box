27dea5eb4c01beb08ae0814ebc40b331d8252f4c
Spencer Herms <slherms@gmail.com>
2018-01-14 21:38:51 -0700

    adds input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Input from './Input.jsx';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cardArray: []
+    };
+  }
   render() {
     return (
       <div className="App">
-      <h1>Hey!</h1>
+      <Input />
       </div>
     );
   }

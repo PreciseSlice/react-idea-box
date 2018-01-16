a1fea67ae9744225e032ea8a8417260d31c09c66
Spencer Herms <slherms@gmail.com>
2018-01-15 20:37:30 -0700

    sets up card class

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 import Input from './Input.jsx';
+import Card from './Card.jsx';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      cardArray: []
+      cardArray: [],
+      cardData: {
+        idea: 'myIdea', 
+        body: 'blah blah blah',
+        quality: 'swill'
+      }
     };
   }
+  
   render() {
     return (
       <div className="App">
       <Input />
+      <Card cardData={this.state.cardData} />
       </div>
     );
   }
